db4712392022dec4d4d16d51313e2ac5633e82a1
Janna Ferdous <niavooa@gmail.com>
2021-10-24 16:41:49 +0100

    Add the index file Add script file Add css stylesheet Add sass stylesheet

---
+++ script.js
@@ -0,0 +1,157 @@
+const quoteBank = [
+  {
+    quote: "Knowing is not enough, we must apply. Willing is not enough, we must do.",
+    author: "Johann Wolfgang von Goethe"
+  },
+  {
+    quote: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",
+    author: "Oprah Winfrey"
+  },
+  {
+    quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    author: "Nelson Mandela"
+  },
+  {
+    quote: "If life were predictable it would cease to be life, and be without flavor.",
+    author: "Eleanor Roosevelt"
+  },
+  {
+    quote: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
+    author: "Steve Jobs"
+  }, 
+  {
+    quote: "Life is either a daring adventure or nothing at all.",
+    author: "Helen Keller"
+  },
+  {
+    quote: "Whoever is happy will make others happy too.",
+    author: "Anne Frank"
+  }
+];
+
+window.onload = init;
+function init() {
+  generateNewQuote();
+  /*console.log('JS Loaded!');
+  document.getElementById("text").innerText = "Wanderlust Coder";
+  document.getElementById("author").innerText = "Janna"; */
+}
+
+function generateNewQuote() {
+  let quoteLength = quoteBank.length;
+  let randomQuoteIndex = Math.floor(Math.random() * quoteLength);
+  let randomQuoteData = quoteBank[randomQuoteIndex];
+
+  let twitterLink = "https://twitter.com/intent/tweet?hashtags=quotes&amp;related=freecodecamp&amp;text=";
+
+  // Add the Quote & replace space with "%20"
+  let quoteInApiFormat = randomQuoteData.quote.replace(/ /g, "%20");
+  twitterLink += quoteInApiFormat;
+  // Add the Author & replace space with "%20"
+  let authorInApiFormat = randomQuoteData.author.replace(/ /g, "%20");
+  twitterLink += " - " + authorInApiFormat;
+
+  document.getElementById("tweet-quote").href = twitterLink;
+  document.getElementById("text").innerText = randomQuoteData.quote;
+  document.getElementById("author").innerText = randomQuoteData.author;
+}
+/*
+const projectName = 'random-quote-machine';
+let quotesData;
+
+var colors = [
+  '#16a085',
+  '#27ae60',
+  '#2c3e50',
+  '#f39c12',
+  '#e74c3c',
+  '#9b59b6',
+  '#FB6964',
+  '#342224',
+  '#472E32',
+  '#BDBB99',
+  '#77B1A9',
+  '#73A857'
+];
+var currentQuote = '',
+  currentAuthor = '';
+
+function getQuotes() {
+  return $.ajax({
+    headers: {
+      Accept: 'application/json'
+    },
+    url:
+      'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
+    success: function (jsonQuotes) {
+      if (typeof jsonQuotes === 'string') {
+        quotesData = JSON.parse(jsonQuotes);
+        console.log('quotesData');
+        console.log(quotesData);
+      }
+    }
+  });
+}
+
+function getRandomQuote() {
+  return quotesData.quotes[
+    Math.floor(Math.random() * quotesData.quotes.length)
+  ];
+}
+
+function getQuote() {
+  let randomQuote = getRandomQuote();
+
+  currentQuote = randomQuote.quote;
+  currentAuthor = randomQuote.author;
+
+  $('#tweet-quote').attr(
+    'href',
+    'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
+      encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
+  );
+
+  
+  $('#tumblr-quote').attr(
+    'href',
+    'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' +
+      encodeURIComponent(currentAuthor) +
+      '&content=' +
+      encodeURIComponent(currentQuote) +
+      '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'
+  ); 
+
+  $('.quote-text').animate({ opacity: 0 }, 500, function () {
+    $(this).animate({ opacity: 1 }, 500);
+    $('#text').text(randomQuote.quote);
+  });
+
+  $('.quote-author').animate({ opacity: 0 }, 500, function () {
+    $(this).animate({ opacity: 1 }, 500);
+    $('#author').html(randomQuote.author);
+  });
+
+  var color = Math.floor(Math.random() * colors.length);
+  $('html body').animate(
+    {
+      backgroundColor: colors[color],
+      color: colors[color]
+    },
+    1000
+  );
+  $('.button').animate(
+    {
+      backgroundColor: colors[color]
+    },
+    1000
+  );
+}
+
+$(document).ready(function () {
+  getQuotes().then(() => {
+    getQuote();
+  });
+
+  $('#new-quote').on('click', getQuote);
+});
+*/
